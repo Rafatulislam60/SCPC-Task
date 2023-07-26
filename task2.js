3cc59f49ccc1bd1b13da301b0cc83e0e26cc28b8
Rafatulislam60 <rafatulislam67@gmail.com>
2023-07-26 20:37:14 +0600

    task2 add

---
+++ task2.js
@@ -0,0 +1,12 @@
+const sumOfPositiveNumbers = (arr) => {
+    let sum = 0;
+
+    for(let num of arr) {
+        if (num > 0){
+            sum += num;
+        }
+    }
+    return sum;
+}
+
+console.log(sumOfPositiveNumbers([2,-5,10,7,-9]));
